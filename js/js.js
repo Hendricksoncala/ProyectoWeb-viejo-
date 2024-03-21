73c93ddc840577ebe9fcd60fc90510f4315c2c3e
Hendricksoncala <156349949+Hendricksoncala@users.noreply.github.com>
2024-03-20 21:20:44 -0500

    feat: :sparkles: se creo el menu desplegable
    se creo el menu desplegable

--- js/js.js
+++ js/js.js
@@ -1,11 +1,4 @@
-const nav = document.querySelector("#nav");
-const abrir= document.querySelector("#abrir");
-const cerrar= document.querySelector("#cerrar");
-
-abrir.addEventListener("click", () => {
-    nav.classList.add("visible");
-})
-
-cerrar.addEventListener("click", () => {
-    nav.classList.remove("visible")
-})
+function abrir() {
+    let menu = document.querySelector('.menu_derecha')
+    menu.classList.toggle('visible')
+}
